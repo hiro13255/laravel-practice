7536c9abc53983b53094d84e6910b17948a42089
hiro13255 <kirakira7573@gmail.com>
2022-07-10 18:24:28 +0900

    booksテーブルのカラム修正

--- practice/database/migrations/2022_07_10_091703_create_books_tables.php
+++ practice/database/migrations/2022_07_10_091703_create_books_tables.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 50);
+            $table->integer('price');
+            $table->string('author', 50)->nullable();
             $table->timestamps();
         });
     }
